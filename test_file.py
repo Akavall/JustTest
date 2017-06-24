2fa8643e2b1f4218cd211db4c556d940789acb72
Akavall <akavall@gmail.com>
2017-06-24 08:40:05 -0700

    added dumb to config

--- test_file.py
+++ test_file.py
@@ -1,9 +1,5 @@
 print 'this is a test 2'
 print 'my line version 1'
 print 'my line 2, very dumb'
-print 'working one...'
-print 'working two...'
-print 'something smart'
 
-print 'test rebase 1'
-print 'test rebase 2'
+print 'major change'
